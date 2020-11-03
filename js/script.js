0009d3df4a301cb4575811b771cb7f22456a3ec0
kiryushkinalexander <70856875+kiryushkinalexander@users.noreply.github.com>
2020-11-04 00:24:51 +0300

    pixel perfect+js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-let button = document.querySelector(".button-primary");
-let form = document.querySelector(".search-form");
-let body = document.querySelector(".page-body");
-let fields = document.querySelectorAll(".form-field-button");
+const button = document.querySelector(".button-primary");
+const form = document.querySelector(".search-form");
+const tripStart = form.querySelector('.trip-start');
+const tripEnd = form.querySelector('.trip-end');
+
 
 button.addEventListener ("click", (event) => {
     event.preventDefault();
     form.classList.toggle("show-form");
 });
 
-form.addEventListener('submit', function (event) {
-  event.preventDefault()
-
-  for (var i = 0; i < fields.length; i++) {
-    if (!fields[i].value) {
-      console.log('field is blank', fields[i])
+form.addEventListener('submit', function (evt) {
+    if (!tripStart.value || !tripEnd.value) {
+      evt.preventDefault();
+      form.classList.remove("form-error");
+      form.offsetWidth = form.offsetWidth;
+      form.classList.add("form-error");
+      console.log('error');
+    } else {
+        return false;
     }
-  }
-})
+  });
