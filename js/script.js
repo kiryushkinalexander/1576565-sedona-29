7792fcf96f4efcb09b0164d7f49d476aae1c6d74
kiryushkinalexander <70856875+kiryushkinalexander@users.noreply.github.com>
2020-11-06 00:07:59 +0300

    правки

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const button = document.querySelector(".button-primary");
-const form = document.querySelector(".search-form");
+const button = document.querySelector('.button-primary');
+const form = document.querySelector('.search-form');
 const tripStart = form.querySelector('.trip-start');
 const tripEnd = form.querySelector('.trip-end');
 const countAdults = form.querySelector('.count-adults');
@@ -9,39 +9,39 @@ let isStorageSupport = true;
 let storage = "";
 
 try {
-  storage = localStorage.getItem("adults" || "children");
+  storage = localStorage.getItem('adults', 'children');
 } catch (err) {
   isStorageSupport = false;
 }
 
 form.classList.add('hide-form');
 
-button.addEventListener ("click", (event) => {
-    event.preventDefault();
-    form.classList.toggle("show-form");
-    form.classList.remove("form-error");
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  form.classList.toggle('show-form');
+  form.classList.remove('form-error');
 });
 
 form.addEventListener('submit', function (evt) {
-    if (!tripStart.value || !tripEnd.value) {
+  if (!tripStart.value || !tripEnd.value || !countAdults.value || !countChildren.value) {
+    evt.preventDefault();
+    form.classList.remove('form-error');
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add('form-error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('adults', countAdults.value || 'children', countChildren.value);
+    }
+  }
+});
+
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (form.classList.contains("show-form")) {
       evt.preventDefault();
+      form.classList.remove("show-form");
       form.classList.remove("form-error");
-      form.offsetWidth = form.offsetWidth;
-      form.classList.add("form-error");
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem("adults", countAdults.value || "children", countChildren.value);
-      } 
     }
-  });
-
-
-  window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (form.classList.contains("show-form")) {
-        evt.preventDefault();
-        form.classList.remove("show-form");
-        form.classList.remove("form-error");
-      }
-    }
-  });
+  }
+});
